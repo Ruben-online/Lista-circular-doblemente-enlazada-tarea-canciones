fb15e67cdc127eb70f10da49483eb67e33a9c8b2
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2024-03-05 09:41:13 -0600

    Update

---
+++ main_playlist.py
@@ -0,0 +1,40 @@
+from doubly_circular_linked_list import DoublyCircularLinkedList
+
+doubly_circular_linked_list = DoublyCircularLinkedList()
+
+while True:
+    title = "Menu reproductor de cancines"
+    print("\n" + title + "\n" + len(title) * '-')
+    print("1. Agregar cancion a la playlist")
+    print("2. Eliminar cancion de la playlist")
+    print("3. Editar la playlist")
+    print("4. Buscar cancion en la playlist")
+    print("5. Cancion anterior")
+    print("6. Siguiente cancion")
+    print("7. Salir del programa")
+
+    playlist_menu = input("\nIngrese una opcion: ")
+
+    if playlist_menu == "1":
+        pass
+
+    elif playlist_menu == "2":
+        pass
+
+    elif playlist_menu == "3":
+        pass
+
+    elif playlist_menu == "4":
+        pass
+
+    elif playlist_menu == "5":
+        pass
+
+    elif playlist_menu == "6":
+        pass
+
+    elif playlist_menu == "7":
+        print("Saliendo del programa...")
+        break
+    else:
+        print("Ingrese una opcion valida")
